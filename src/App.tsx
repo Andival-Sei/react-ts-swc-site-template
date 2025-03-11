5df12c71e11790721a7f97470ce33203995e2962
Kirill Barkov <freedomdragon777@gmail.com>
2025-03-12 00:18:01 +0400

    Добавить ленивую загрузку страниц с использованием React.lazy и Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
-// import styled, { ThemeProvider } from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import Header from "./components/header/Header";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import { theme } from "./styles/theme";
+import React, { Suspense } from "react";
 
-// const Container = styled.div`
-//   padding: 2rem;
-// `;
 // Импорт страниц
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contacts from "./pages/Contacts";
+const Home = React.lazy(() => import("./pages/Home"));
+const About = React.lazy(() => import("./pages/About"));
+const Contacts = React.lazy(() => import("./pages/Contacts"));
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
-        {/* <Container> */}
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contacts" element={<Contacts />} />
-        </Routes>
-        {/* </Container> */}
+        <Suspense fallback={<div>Загрузка...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Routes>
+        </Suspense>
       </ThemeProvider>
     </BrowserRouter>
   );
